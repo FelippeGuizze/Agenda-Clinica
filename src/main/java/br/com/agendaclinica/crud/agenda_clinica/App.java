f022b60ec75c414b70e9e841d496fecc5891dfbd
FelippeGuizze <eunaoodeiorato@gmail.com>
2026-04-07 14:14:03 -0300

    First Commit

---
+++ src/main/java/br/com/agendaclinica/crud/agenda_clinica/App.java
@@ -0,0 +1,10 @@
+package br.com.agendaclinica.crud.agenda_clinica;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
